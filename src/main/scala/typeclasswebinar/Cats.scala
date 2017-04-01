def8a474b2eeb0588bb0eee393677a448ea86a1e
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2017-04-01 16:08:30 +0300

    Examples simplified

--- src/main/scala/typeclasswebinar/Cats.scala
+++ src/main/scala/typeclasswebinar/Cats.scala
@@ -1,17 +1,35 @@
 package typeclasswebinar.e3.cats
 
 import cats._
-import cats.instances.all._
 import cats.syntax.all._
 
-object E9_Cats extends App {
-  case class Record[A](firstName: String, lastName: String, phone: A)
+object Phonebook {
+  case class Record(firstName: String, lastName: String, phone: String)
 
-  implicit val phoneFunctor: Functor[Record] = new Functor[Record] {
-    def map[A, B](r: Record[A])(f: A => B): Record[B] = r.copy(phone = f(r.phone))
+  implicit val recordShow: Show[Record] = new Show[Record] {
+    def show(x: Record) = s"""{"first_name": "${x.firstName}", "last_name": "${x.firstName}", "phone": "${x.phone}"}"""
+  }
+}
+import Phonebook._
+
+// Single record serialization
+object E5_RecordSingle extends App {
+  val record = Record("John", "Smith", "0123456789")
+  println(record.show)
+}
+
+// List of records serialization
+object E6_RecordsList extends App {
+  implicit def listShow[T](implicit typeclass: Show[T]): Show[List[T]] = new Show[List[T]] {
+    def show(x: List[T]): String =
+      s"""[
+         |   ${x.map { e => e.show }.mkString(",\n  ")}
+         |]""".stripMargin
   }
 
-  val record = Record("John", "Smith", "0123456789")
-  val mapped = record.map { p => s"+1$p" }
-  println(mapped)
+  val records = List(
+    Record("John", "Smith"  , "0123456789"),
+    Record("Tom" , "Johnson", "9876543210")
+  )
+  println(records.show)
 }
--- src/main/scala/typeclasswebinar/Typeclass.scala
+++ src/main/scala/typeclasswebinar/Typeclass.scala
@@ -1,109 +1,37 @@
 package typeclasswebinar.e2.typeclass
 
 object Phonebook {
-  object typeclasses {
-    trait ToJson[T] { def toJson(x: T): String }
-    trait ToXml [T] { def toXml (x: T): String }
-    trait ToCSV [T] { def toCSV (x: T): String }
-  }
+  trait ToJson[T] { def toJson(x: T): String }
 
-  object data {
-    case class Record(firstName: String, lastName: String, phone: String)
-  }
+  implicit class ToJsonSyntax[T](x: T)(implicit typeclass: ToJson[T])
+    { def toJson: String = typeclass.toJson(x) }
 
-  object instances {
-    import typeclasses._
+  case class Record(firstName: String, lastName: String, phone: String)
 
-    object record extends record
-    trait  record {
-      import data.Record
-
-      implicit val recordToJson: ToJson[Record] = new ToJson[Record] {
-        def toJson(x: Record): String = s"""{"first_name": "${x.firstName}", "last_name": "${x.firstName}", "phone": "${x.phone}"}"""
-      }
-
-      implicit val recordToXml: ToXml[Record] = new ToXml[Record] {
-        def toXml(x: Record): String = s"""<record><first_name>${x.firstName}</first_name><last_name>${x.firstName}</last_name><phone>${x.phone}</phone></record>"""
-      }
-
-      implicit val recordToCSV: ToCSV[Record] = new ToCSV[Record] {
-        def toCSV(x: Record): String = s"""${x.firstName},${x.firstName},${x.phone}"""
-      }
-    }
-
-    object list extends list
-    trait list {
-      implicit def listToJson[T](implicit typeclass: ToJson[T]): ToJson[List[T]] = new ToJson[List[T]] {
-        def toJson(x: List[T]): String =
-          s"""[
-             |   ${x.map { e => typeclass.toJson(e)}.mkString("\n,  ")}
-             |]""".stripMargin
-      }
-    }
-
-    object all extends record with list
-  }
-
-  object syntax {
-    import typeclasses._
-
-    implicit class ToToJsonSyntax[T](x: T)(implicit typeclass: ToJson[T])
-      { def toJson: String = typeclass.toJson(x) }
-
-    implicit class ToToXmlSyntax [T](x: T)(implicit typeclass: ToXml [T])
-      { def toXml : String = typeclass.toXml (x) }
-
-    implicit class ToToCSVSyntax [T](x: T)(implicit typeclass: ToCSV [T])
-      { def toCSV : String = typeclass.toCSV (x) }
+  implicit val recordToJson: ToJson[Record] = new ToJson[Record] {
+    def toJson(x: Record): String = s"""{"first_name": "${x.firstName}", "last_name": "${x.firstName}", "phone": "${x.phone}"}"""
   }
 }
-
-object AddressBook {
-  case class Address(firstName: String, lastName: String, address: String)
-}
-
-import Phonebook.typeclasses._
-import Phonebook.data._
-import Phonebook.instances.all._
-import Phonebook.syntax._
+import Phonebook._
 
 // Single record serialization
-object E5_RecordsSingle extends App {
+object E3_RecordSingle extends App {
   val record = Record("John", "Smith", "0123456789")
-  println(s"${record.toJson}\n${record.toXml}\n${record.toCSV}")
+  println(record.toJson)
 }
 
 // List of records serialization
-object E6_RecordsList extends App {
+object E4_RecordsList extends App {
+  implicit def listToJson[T](implicit typeclass: ToJson[T]): ToJson[List[T]] = new ToJson[List[T]] {
+    def toJson(x: List[T]): String =
+      s"""[
+         |  ${x.map { e => e.toJson }.mkString(",\n  ")}
+         |]""".stripMargin
+  }
+
   val records = List(
     Record("John", "Smith"  , "0123456789"),
     Record("Tom" , "Johnson", "9876543210")
   )
   println(records.toJson)
-}
-
-// 3rd party framewrok added: Address book
-import AddressBook._
-
-// Type classes for Address
-trait AddressTypeclasses {
-  implicit val addressToJson: ToJson[Address] = new ToJson[Address] {
-    def toJson(x: Address): String =
-      s"""{"first_name": "${x.firstName}", "last_name": "${x.firstName}", "address": "${x.address}"}"""
-  }  
-}
-
-// Single address
-object E7_AddressesSingle extends App with AddressTypeclasses {
-  val address = Address("John", "Smith", "1600 Pennsylvania Ave NW, Washington, DC 20500, USA")
-  println(address.toJson)
-}
-
-// List of addresses
-object E8_AddressList extends App with AddressTypeclasses {
-  val addresses = List(
-    Address("John", "Smith", "1600 Pennsylvania Ave NW, Washington, DC 20500, USA"),
-    Address("Tom", "Johnson", "Fort Meade, MD 20755-6248")
-  )
-  println(addresses.toJson)
 }
